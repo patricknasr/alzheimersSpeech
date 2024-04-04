83650f75a77b548f361c35d6377b7fb782b6c76a
Patrick Nasr <nasr7601@hotmail.com>
2024-04-04 23:36:46 +1100

    correlation image updated

--- src/correlation.py
+++ src/correlation.py
@@ -8,7 +8,7 @@ def process_new_data(features_file_path):
     features = ['F0semitoneFrom27.5Hz_sma3nz', 'jitterLocal_sma3nz', 'shimmerLocaldB_sma3nz'] 
     correlation_matrix = df[features].corr()
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
     plt.title('Correlation Heatmap of Selected Speech Features')
     plt.show()
